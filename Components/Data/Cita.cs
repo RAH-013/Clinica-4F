9faf2e3baf8f4562d911dd4d82044b7ed5a2e6b5
RAH-013 <ranguiano7@ucol.mx>
2025-05-27 21:23:36 -0600

    Version 2

--- Components/Data/Cita.cs
+++ Components/Data/Cita.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Clinica.Components.Data
 {
@@ -7,19 +7,20 @@ namespace Clinica.Components.Data
     {
         [Key]
         public string IdCita { get; set; } = string.Empty;
+        [ForeignKey(nameof(IdPaciente))]
+        [Required(ErrorMessage = "El campo \"Paciente\" no puede quedar vacio.")]
         public string IdPaciente { get; set; } = string.Empty;
         public string Paciente { get; set; } = string.Empty;
+        [ForeignKey(nameof(IdMedico))]
+        [Required(ErrorMessage = "El campo \"Medico\" no puede quedar vacio.")]
         public string IdMedico { get; set; } = string.Empty;
         public string Medico { get; set; } = string.Empty;
-
         [Required(ErrorMessage = "El campo \"Fecha de la Cita\" no puede quedar vacío.")]
+        [Range(typeof(DateTime), "01/01/2025", "31/12/2025", ErrorMessage = "Fecha fuera de rango")]
         public DateTime FechaCita { get; set; }
 
-        [Required(ErrorMessage = "El campo \"Hora de inicio\" no puede quedar vacío.")]
+        [Required(ErrorMessage = "El campo \"Hora\" no puede quedar vacío.")]
         public TimeSpan HoraInicio { get; set; }
-
-        [Required(ErrorMessage = "El campo \"Hora de fin\" no puede quedar vacío.")]
-        public TimeSpan HoraFin { get; set; }
 
         [Required(ErrorMessage = "El campo \"Estado\" no puede quedar vacío.")]
         [RegularExpression("pendiente|confirmada|cancelada|completada", ErrorMessage = "El estado no es válido.")]
